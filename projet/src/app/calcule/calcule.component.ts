2aa9e242ac8081a26fd54096a713357985cbb08a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-29 16:31:33 +0100

    commit

--- projet/src/app/calcule/calcule.component.ts
+++ projet/src/app/calcule/calcule.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calcule.component.css']
 })
 export class CalculeComponent implements OnInit {
-
+  sb = 0;
+  sn = 0;
+  taxe=0
   constructor() { }
 
   ngOnInit(): void {
+  }
+  calculer() {
+    this.taxe = this.sb * 0.12;
+    this.sn = this.sb * 0.88;
+
+
+
+
+
   }
 
 }
